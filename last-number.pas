d4bdbc16ff1f05413d845a029435e9a4403b5383
lizoksia <lizoksia@github.com>
2022-05-15 00:01:40 +0300

    добавлена программа "последнее число"

---
+++ last-number.pas
@@ -0,0 +1,12 @@
+﻿program last_number;
+var a,b,c:string;
+begin
+  read(a);
+if length(a)<=9 then
+write(rightstr(a,1))
+else
+  if length(a)<=189 then
+  write(rightstr(a,2))
+else
+  write(rightstr(a,3));
+end.
